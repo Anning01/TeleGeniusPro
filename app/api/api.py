f666eef86f4c7461f16a6d3b74d12dee59ca4382
LiuJiaLe-git <2944128670@qq.com>
2025-05-14 10:11:57 +0800

    add dataset_manage

--- app/api/api.py
+++ app/api/api.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter
 
 from app.api.endpoints import users, scripts
 from app.ragflow import pan_card_dataset
+from app.ragflow import dataset_manage
+
 api_router = APIRouter()
+
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 # api_router.include_router(scripts.router, prefix="/scripts", tags=["scripts"])
 api_router.include_router(pan_card_dataset.router, prefix="/ragflow", tags=["ragflow"])
+api_router.include_router(dataset_manage.router, prefix="/ragflow", tags=["ragflow"])
